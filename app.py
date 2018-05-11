4ac3f0e48e89a7c872526f6b4ed59f53d63545d3
Tejas Pancholi <tejas.er@outlook.com>
2018-05-11 15:01:37 +0530

    removed creds.

--- app.py
+++ app.py
@@ -16,9 +16,9 @@ from flask_login import LoginManager, UserMixin, login_required, logout_user, cu
 # print a nice greeting.
 
 FIELDS = {}
-SQL_URI = "sqlite:///C:\\Users\\netsystem\\Documents\\projects\\report\\database.db"
-MONGO_URI = "mongodb://tejas:tejas@ds161793.mlab.com:61793/tejas-social"
-SECRET = 'thisissupposedtobeasecretmessage.'
+SQL_URI = ""
+MONGO_URI = ""
+SECRET = ''
 
 # EB looks for an 'application' callable by default.
 application = Flask(__name__)
@@ -30,31 +30,42 @@ login_manager = LoginManager()
 login_manager.init_app(application)
 login_manager.login_view = 'login'
 
+
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(15), unique=True)
     email = db.Column(db.String(50), unique=True)
     password = db.Column(db.String(80))
 
+
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
 
+
 class loginForm(FlaskForm):
-    username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
-    password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
+    username = StringField('username', validators=[
+                           InputRequired(), Length(min=4, max=15)])
+    password = PasswordField('password', validators=[
+                             InputRequired(), Length(min=8, max=80)])
     remember = BooleanField('remember me')
 
+
 class RegistrationForm(FlaskForm):
-    email = StringField('email', validators=[InputRequired(), Email(message='Invalid email'), Length(max=50)])
-    username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
-    password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
+    email = StringField('email', validators=[InputRequired(), Email(
+        message='Invalid email'), Length(max=50)])
+    username = StringField('username', validators=[
+                           InputRequired(), Length(min=4, max=15)])
+    password = PasswordField('password', validators=[
+                             InputRequired(), Length(min=8, max=80)])
 
 # connect to mongolab -- all database credentials in a 'credens' file
+
 
 @application.route('/')
 def index():
     return render_template('index.html')
+
 
 @application.route('/login', methods=['GET', 'POST'])
 def login():
@@ -70,13 +81,16 @@ def login():
 
     return render_template('login.html', form=form)
 
+
 @application.route('/signup', methods=['GET', 'POST'])
 @login_required
 def signup():
     form = RegistrationForm()
     if form.validate_on_submit():
-        hashed_password = generate_password_hash(form.password.data, method='sha256')
-        new_user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        hashed_password = generate_password_hash(
+            form.password.data, method='sha256')
+        new_user = User(username=form.username.data,
+                        email=form.email.data, password=hashed_password)
         db.session.add(new_user)
         db.session.commit()
         return '<h1>New user has been created</h1>'
@@ -84,11 +98,13 @@ def signup():
 
     return render_template('signup.html', form=form)
 
+
 @application.route('/logout')
 @login_required
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
 
 @application.route('/home')
 @login_required
@@ -104,6 +120,7 @@ def home():
         # companies = json.dumps(companies, default=json_util.default)
         connection.close()
     return render_template("home.html", companies=companies, name=current_user.username)
+
 
 @application.route('/summary', methods=['GET', 'POST'])
 @login_required
@@ -122,7 +139,8 @@ def summary(account=None):
         # json_projects = json.dumps(json_projects, default=json_util.default)
         connection.close()
     cm_ga_rev,  cm_fb_rev, lm_ga_rev, lm_fb_rev = [0.0 for _ in range(4)]
-    lm_fb_reach, lm_fb_clicks, lm_fb_impression, cm_fb_reach, cm_fb_clicks, cm_fb_impression = [0 for _ in range(6)]
+    lm_fb_reach, lm_fb_clicks, lm_fb_impression, cm_fb_reach, cm_fb_clicks, cm_fb_impression = [
+        0 for _ in range(6)]
     lm_ga_orders, cm_ga_orders = [0 for _ in range(2)]
     for item in json_projects:
         if item['Comparision'] == 'CM':
@@ -133,22 +151,25 @@ def summary(account=None):
             cm_fb_clicks = item['Link_clicks']
             cm_fb_impression = item['Impressions']
         elif item['Comparision'] == 'LM':
-            lm_ga_rev        = item['GA_Revenue']
-            lm_ga_orders     = item['GA_orders']
-            lm_fb_rev        = item['FB_Revenue']
-            lm_fb_reach      = item['Reach']
-            lm_fb_clicks     = item['Link_clicks']
+            lm_ga_rev = item['GA_Revenue']
+            lm_ga_orders = item['GA_orders']
+            lm_fb_rev = item['FB_Revenue']
+            lm_fb_reach = item['Reach']
+            lm_fb_clicks = item['Link_clicks']
             lm_fb_impression = item['Impressions']
     if((lm_ga_rev == 0.0) or (lm_fb_rev == 0.0) or (lm_fb_reach == 0) or (lm_fb_clicks == 0) or (lm_fb_impression == 0) or (lm_ga_orders == 0)):
-        ga_rev_change, ga_orders_change, fb_rev_change, fb_reach_change, fb_clicks_change, fb_impression_change = ['NA' for _ in range(6)]
+        ga_rev_change, ga_orders_change, fb_rev_change, fb_reach_change, fb_clicks_change, fb_impression_change = [
+            'NA' for _ in range(6)]
     else:
-        ga_rev_change        = ((cm_ga_rev - lm_ga_rev)/lm_ga_rev) * 100
-        ga_orders_change     = ((cm_ga_orders - lm_ga_orders)/lm_ga_orders) * 100
-        fb_rev_change        = ((cm_fb_rev - lm_fb_rev)/lm_fb_rev) * 100
-        fb_reach_change      = ((cm_fb_reach - lm_fb_reach)/lm_fb_reach) * 100
-        fb_clicks_change     = ((cm_fb_clicks - lm_fb_clicks)/lm_fb_clicks) * 100
-        fb_impression_change = ((cm_fb_impression - lm_fb_impression)/lm_fb_impression) * 100
-    return render_template("summary.html", account=account, name=current_user.username, lm_fb_impression=lm_fb_impression, cm_fb_impression=cm_fb_impression, lm_fb_clicks=lm_fb_clicks, cm_fb_clicks=cm_fb_clicks, lm_fb_reach=lm_fb_reach, cm_fb_reach=cm_fb_reach, fb_impression_change=fb_impression_change, fb_clicks_change=fb_clicks_change, fb_reach_change=fb_reach_change, ga_orders_change=ga_orders_change, ga_rev_change=ga_rev_change, fb_rev_change=fb_rev_change,cm_ga_rev=cm_ga_rev, lm_ga_rev=lm_ga_rev, cm_ga_orders=cm_ga_orders, lm_ga_orders=lm_ga_orders, lm_fb_rev=lm_fb_rev, cm_fb_rev=cm_fb_rev)
+        ga_rev_change = ((cm_ga_rev - lm_ga_rev)/lm_ga_rev) * 100
+        ga_orders_change = ((cm_ga_orders - lm_ga_orders)/lm_ga_orders) * 100
+        fb_rev_change = ((cm_fb_rev - lm_fb_rev)/lm_fb_rev) * 100
+        fb_reach_change = ((cm_fb_reach - lm_fb_reach)/lm_fb_reach) * 100
+        fb_clicks_change = ((cm_fb_clicks - lm_fb_clicks)/lm_fb_clicks) * 100
+        fb_impression_change = (
+            (cm_fb_impression - lm_fb_impression)/lm_fb_impression) * 100
+    return render_template("summary.html", account=account, name=current_user.username, lm_fb_impression=lm_fb_impression, cm_fb_impression=cm_fb_impression, lm_fb_clicks=lm_fb_clicks, cm_fb_clicks=cm_fb_clicks, lm_fb_reach=lm_fb_reach, cm_fb_reach=cm_fb_reach, fb_impression_change=fb_impression_change, fb_clicks_change=fb_clicks_change, fb_reach_change=fb_reach_change, ga_orders_change=ga_orders_change, ga_rev_change=ga_rev_change, fb_rev_change=fb_rev_change, cm_ga_rev=cm_ga_rev, lm_ga_rev=lm_ga_rev, cm_ga_orders=cm_ga_orders, lm_ga_orders=lm_ga_orders, lm_fb_rev=lm_fb_rev, cm_fb_rev=cm_fb_rev)
+
 
 @application.route('/fbdata', methods=['GET'])
 @login_required
@@ -159,17 +180,18 @@ def viewdbfile(account=None):
     account = request.args.get('account')
     account = account.replace('_', ' ')
     results = db.find(
-        {'Account': account, 'Comparision' : 'CM'})
+        {'Account': account, 'Comparision': 'CM'})
     json_projects = []
     if results:
         for result in results:
             json_projects.append(result)
         json_projects = json.dumps(json_projects, default=json_util.default)
         connection.close()
-        
+
         return json_projects
     else:
         return 'error reading file..'
+
 
 @application.route('/detail',  methods=['GET'])
 @login_required
@@ -177,7 +199,6 @@ def getDetailPage(account=None):
     account = request.args.get('account')
     account = account.replace('_', ' ')
     return render_template("detail.html", account=account, name=current_user.username)
-
 
 
 if __name__ == "__main__":
